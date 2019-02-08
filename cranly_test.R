b82684816310b32e647284f03a217515aca2c044
Matt Dray <mwdray@gmail.com>
2019-02-08 23:58:38 +0000

    Get author combos and tidygraph

--- cranly_test.R
+++ cranly_test.R
@@ -6,6 +6,8 @@ library(dplyr)
 library(tibble)
 library(tidyr)
 library(tools)
+library(tidygraph)
+library(purrr)
 
 # Database
 
@@ -25,3 +27,25 @@ pkg_aut %>%
   count(author) %>% 
   arrange(desc(n)) %>% 
   top_n(10, n)
+
+# Small sample to test with
+
+pkg_aut_small <- pkg_aut %>%
+  filter(package %in% c("dplyr", "readr"))
+
+# Within each packagem get author combos
+# https://www.williamrchase.com/post/finding-combinations-in-the-tidyverse/
+# How to reattach the package name?
+
+pkg_aut_graph <- pkg_aut_small %>% 
+  group_by(package) %>%
+  filter(n() > 1) %>%
+  split(.$package) %>%
+  map(., 2) %>%
+  map(~combn(.x, m = 2)) %>%
+  map(~t(.x)) %>%
+  map_dfr(as_tibble)
+
+# Build tidygraph
+
+pkg_aut_graph <- as_tbl_graph(pkg_aut_graph, directed = FALSE)
