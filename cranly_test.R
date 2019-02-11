1ceb0bebeae06d2552b61b33a15d0f46c2b35efa
Matt Dray <mwdray@gmail.com>
2019-02-11 22:33:05 +0000

    Retain package name with edges, label edges

--- cranly_test.R
+++ cranly_test.R
@@ -26,21 +26,27 @@ pkg_aut <- pkg_db %>%
   select(package, author) %>% 
   unnest()
 
-# Top authors
+# Authors with largest number of packages
 
 pkg_aut %>% 
   count(author) %>% 
   arrange(desc(n)) %>% 
   top_n(10, n)
 
+# Packages with largest number of authors
+
+pkg_aut %>% 
+  count(package) %>% 
+  arrange(desc(n)) %>% 
+  top_n(10, n)
+
 # Small sample to test with
 
 pkg_aut_small <- pkg_aut %>%
-  filter(package %in% c("dplyr", "readr", "purrr", "lme4"))
+  filter(package %in% c("dplyr", "readr", "purrr", "magrittr"))
 
 # Within each package, get author combos
 # https://www.williamrchase.com/post/finding-combinations-in-the-tidyverse/
-# How to reattach the package name?
 
 pkg_aut_graph <- pkg_aut_small %>% 
   group_by(package) %>%
@@ -49,7 +55,9 @@ pkg_aut_graph <- pkg_aut_small %>%
   map(., 2) %>%
   map(~combn(.x, m = 2)) %>%
   map(~t(.x)) %>%
-  map_dfr(as_tibble)
+  map(as_tibble) %>%
+  bind_rows(.id = "package") %>% 
+  select(V1, V2, package)
 
 # Build tidygraph
 
@@ -66,7 +74,12 @@ graph_pop <- pkg_aut_graph %>%
   arrange(desc(Popularity))
 
 # Call plot
-
+# https://www.data-imaginist.com/2017/ggraph-introduction-edges/
+  
 ggraph(graph_pop, layout = "kk") + 
-  geom_edge_link() + 
+  geom_edge_fan(  # edges between same ndoes are separated
+    aes(label = package),
+    angle_calc = 'along',
+    label_dodge = unit(2.5, 'mm')
+  ) + 
   geom_node_point(aes(size = Popularity))
