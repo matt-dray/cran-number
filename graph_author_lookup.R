a6779d360c60e31fe8eabb270d802d78c68012ba
matt-dray <mwdray@gmail.com>
2019-02-26 22:22:45 +0000

    Make tbl of aut, HW tbl_graph, separation

--- graph_author_lookup.R
+++ graph_author_lookup.R
@@ -1,13 +1,12 @@
-# The SHiny app reacts when a name is selected. It subsets the CRAN-wide 
+# The Shiny app reacts when a name is selected. It subsets the CRAN-wide 
 # tidygraph to produce a graph of the shortest path between person A and B.
-# This is slow.
+# This is relatively slow. Or is it?
 
 # How big would a tibble be if it was one row per author, with a column for
 # author name; a column containing a tidygraph object from them to HW; and a
 # column with the distance? The Shiny app could then simply select the
-# appropriate author-to-HW tidygraph to display. Like this:
-#
-# author | tidygraph | hadley_number
+# appropriate author-to-HW tidygraph to display. Like this:s
+# author (char) | hadley_graph (tbl_graph) | hadley_number (num)
 
 library(kevinbacran)
 library(dplyr)
@@ -16,7 +15,7 @@ library(cranly)
 library(tidyr)
 
 # Prepare combos tidygraph
-combo_graph<- kb_combos()
+combo_graph<- kevinbacran::cran_pkg_graph
 
 # Tibble of package-author combos
 pkg_auth <- tools::CRAN_package_db() %>%
@@ -36,19 +35,31 @@ auth <- pkg_auth %>%
 # Test --------------------------------------------------------------------
 
 
-# Small test set
-auth_test <- filter(auth, author %in% c("Jenny Bryan", "Joanna Zhao", "Yihui Xie", "Joe Cheng")) %>% 
-  mutate(biggraph = list(combo_graph))
-
 # To each row create a tidygraph object using the author name
 test <- auth_test %>% 
-  mutate(minigraph = map2(biggraph, author_name, ~kb_pair(tidy_graph = .x, name_a = .y)))
+  mutate(
+    hadley_graph = map(author_name, ~kb_pair(tidy_graph = combo_graph, name_a = .x)),
+    hadley_separation = unlist(map(hadley_graph, ~kb_distance(.)))
+  )
+
+# Expose an example tbl_graph
+test$hadley_separation[test$author_name == "Jenny Bryan"]
 
 
 # Full set ----------------------------------------------------------------
 
+
+# Get tibble of authors
+# with listcol of tbl_graph to Hadley and column of separation values
 hw_graphs <- auth %>%
   mutate(
-    cran_graph = list(combo_graph),
-    minigraph = map2(cran_graph, author_name, ~kb_pair(tidy_graph = .x, name_a = .y))
-  )
+    hadley_graph = map(author_name, ~kb_pair(tidy_graph = combo_graph, name_a = .x)),
+    hadley_separation = unlist(map(hadley_graph, ~kb_distance(.)))
+  ) 
+
+# Remove empty graphs
+hw_graphs_no_error <- hw_graphs %>% filter(hadley_separation > 0)
+
+# Save these
+saveRDS(hw_graphs, "data/hw_graphs.RDS")
+saveRDS(hw_graphs_no_error, "data/hw_graphs_no_error.RDS")
