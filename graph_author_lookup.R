9ff12f1a0851033e7f2e1a78f45d288d31b726eb
Matt Dray <mwdray@gmail.com>
2019-03-11 18:13:43 +0000

    Make graph_author_lookup readable, fixes #8

--- graph_author_lookup.R
+++ graph_author_lookup.R
@@ -1,23 +1,26 @@
-# The Shiny app reacts when a name is selected. It subsets the CRAN-wide 
-# tidygraph to produce a graph of the shortest path between person A and B.
-# This is relatively slow. Or is it?
+# Author lookup for use in Shiny app
+# Matt Dray
+# Feb 2019
 
-# How big would a tibble be if it was one row per author, with a column for
-# author name; a column containing a tidygraph object from them to HW; and a
-# column with the distance? The Shiny app could then simply select the
-# appropriate author-to-HW tidygraph to display. Like this:s
-# author (char) | hadley_graph (tbl_graph) | hadley_number (num)
+# App URL: https://mattdray.shinyapps.io/hadley-number/ (subject to removal)
+# Blog post: https://www.rostrum.blog/2019/02/27/hadley-number/
 
+# Create a tibble with one row per author. Columns for (1) author name, (2) 
+# a tbl_graph object of the shortest-path graph from that author to HW, and (3)
+# the separation value. The Shiny app then simply selects the appropriate
+# author-to-HW tidygraph to display given an input.
+
+# Load packages
 library(kevinbacran)
 library(dplyr)
 library(purrr)
 library(cranly)
 library(tidyr)
 
-# Prepare combos tidygraph
-combo_graph<- kevinbacran::cran_pkg_graph
+# Use example CRAN graph snapshot dataset that comes with {kecinbacran}
+combo_graph <- kevinbacran::cran_graph
 
-# Tibble of package-author combos
+# Tibble with one row per package-author combo
 pkg_auth <- tools::CRAN_package_db() %>%
   clean_CRAN_db() %>% 
   as_tibble() %>%
@@ -32,43 +35,64 @@ auth <- pkg_auth %>%
   rename(author_name = author) 
 
 
-# Test --------------------------------------------------------------------
 
+
+# Test: shortest path listcol tibble and separation  ----------------------
+
+
+# Get random sample of authors to work with
+auth_test <- sample_n(auth, 10)
 
 # To each row create a tidygraph object using the author name
-test <- auth_test %>% 
+# Uses the test set of authors only
+listcol_tibble <- auth_test %>% 
   mutate(
-    hadley_graph = map(author_name, ~kb_pair(tidy_graph = combo_graph, name_a = .x)),
+    hadley_graph = map(
+      author_name,
+      ~kb_pair(tidy_graph = combo_graph, name_a = .x)
+    ),
     hadley_separation = unlist(map(hadley_graph, ~kb_distance(.)))
   )
 
+# Warnings:
+# In shortest_paths(graph, from = from, to = to, mode = mode, weights = weights,  :
+# At structural_properties.c:745 :Couldn't reach some vertices
+
 # Expose an example tbl_graph
-test$hadley_separation[test$author_name == "Jenny Bryan"]
+# Adjust index value for an author that has hadley_separation >0
+listcol_tibble$hadley_graph[1]
+
+# And extract the separation value
+listcol_tibble$hadley_separation[1]
 
 
-# Full set ----------------------------------------------------------------
+# Expand test to full author dataset --------------------------------------
 
 
-# Get tibble of authors
-# with listcol of tbl_graph to Hadley and column of separation values
+# To each row create a tidygraph object using the author name
+# Uses all authors in CRAN database
 hw_graphs <- auth %>%
   mutate(
-    hadley_graph = map(author_name, ~kb_pair(tidy_graph = combo_graph, name_a = .x)),
+    hadley_graph = map(
+      author_name,
+      ~kb_pair(tidy_graph = combo_graph, name_a = .x)
+    ),
     hadley_separation = unlist(map(hadley_graph, ~kb_distance(.)))
   ) 
 
 # Remove empty graphs
+# Those with zero distance were subject to warnings
 hw_graphs_no_error <- hw_graphs %>% filter(hadley_separation > 0)
 
-# Save these
+# Save these for later use
 saveRDS(hw_graphs, "data/hw_graphs.RDS")
-saveRDS(hw_graphs_no_error, "data/hw_graphs_no_error.RDS")
+saveRDS(hw_graphs_no_error, "data/hw_graphs_no_error.RDS")  # used in Shiny app
 
 
-# Plot --------------------------------------------------------------------
+# Test: plot --------------------------------------------------------------
 
-# kb_plot
-#hw_graphs$hadley_graph[hw_graphs$author_name == "Franz Rubel"][[1]] %>%
+
+# Test a basic plot
 hw_graphs$hadley_graph[hw_graphs$author_name == "Duncan Gormansway"][[1]] %>%
   ggraph(layout = "nicely") +
   geom_edge_fan(
